7d7a6529085d8e084f2a0b6aa3b3a3f7173f434e
shafayeatt1995 <shafayetalanik@gmail.com>
2025-07-03 05:24:41 +0600

    pwa updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,56 @@ export default defineNuxtConfig({
       API_URL: process.env.API_URL,
     },
   },
-  modules: ["@nuxt/fonts", "shadcn-nuxt"],
+  modules: ["@nuxt/fonts", "shadcn-nuxt", "@vite-pwa/nuxt"],
   shadcn: { prefix: "", componentDir: "./components/ui" },
   css: ["~/assets/css/tailwind.css"],
   vite: { plugins: [tailwindcss()] },
+  pwa: {
+    registerType: "autoUpdate",
+    strategies: "generateSW",
+    includeAssets: ["logo.png"],
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.destination === "image",
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 * 30,
+            },
+          },
+        },
+      ],
+    },
+    manifest: {
+      name: "Skyinfo Online",
+      short_name: "Skyinfo",
+      description: "Skyinfo Online",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        {
+          src: "/logo.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/logo.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "/logo.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+  },
 });
