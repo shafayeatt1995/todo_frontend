9390061bb128bd4f9fb09106e336d486ca141fb7
shafayeatt1995 <shafayetalanik@gmail.com>
2025-07-03 05:30:26 +0600

    update pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,12 +53,6 @@ export default defineNuxtConfig({
           sizes: "512x512",
           type: "image/png",
         },
-        {
-          src: "/logo.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any maskable",
-        },
       ],
     },
   },
