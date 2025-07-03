be3a6531c2e20b90f0a7f4dcf9a18e4e308ccb2d
shafayeatt1995 <shafayetalanik@gmail.com>
2025-07-03 18:14:39 +0600

    working

--- pages/dashboard/settings.vue
+++ pages/dashboard/settings.vue
@@ -4,14 +4,18 @@
       <div class="flex justify-between items-center">
         <h1 class="text-2xl font-bold">Settings</h1>
       </div>
-      <div class="flex justify-between items-center">
+      <div class="flex justify-between items-center mb-2">
         <p>Connect Notification</p>
-        <Button @click="connect"> Connect </Button>
+        <Button @click="connect" :disabled="connectLoading">
+          <Loader2Icon v-if="connectLoading" class="animate-spin" />
+          Connect
+        </Button>
       </div>
-      <div class="flex justify-between items-center">
+      <div class="flex justify-between items-center mb-2">
         <p>Check Notification</p>
-        <Button @click="check">
-          <BellIcon />
+        <Button @click="check" :disabled="checkLoading">
+          <Loader2Icon v-if="checkLoading" class="animate-spin" />
+          Check
         </Button>
       </div>
     </div>
@@ -25,9 +29,16 @@ import { toast } from "vue-sonner";
 export default {
   name: "ConnectNotification",
   components: { BellIcon },
+  data() {
+    return {
+      connectLoading: false,
+      checkLoading: false,
+    };
+  },
   methods: {
     async connect() {
       try {
+        this.connectLoading = true;
         const { requestPermission, listenForeground } = useNotifications();
         const token = await requestPermission();
         await this.$api.post("/dashboard/add-fcm", { token });
@@ -36,15 +47,20 @@ export default {
       } catch (err) {
         console.error("❌ Notification setup failed:", err);
         toast.error("Notification permission denied.");
+      } finally {
+        this.connectLoading = false;
       }
     },
     async check() {
       try {
+        this.checkLoading = true;
         await this.$api.get("/dashboard/check-fcm");
         toast.success("Notification checked successfully.");
       } catch (err) {
         console.error("❌ Notification check failed:", err);
         toast.error("Notification check failed.");
+      } finally {
+        this.checkLoading = false;
       }
     },
   },
